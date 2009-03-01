22b2efd12d0775f7496b1d3cef8f3edc8fab154c
Douglas Rudd <drudd@uchicago.edu>
2009-03-01 23:03:29 +0000

    Fix for typo in previous fix.

--- src/io.c
+++ src/io.c
@@ -2485,7 +2485,7 @@ void read_particles( char *header_filename, char *data_filename,
 			} else 	if ( size != num_particles_total * sizeof(int) ) {
 				reorder( (char *)&size, sizeof(int) );
 
-				if ( num_particles_tosize != num_particles_total * sizeof(int) ) {
+				if ( size != num_particles_total * sizeof(int) ) {
 					cart_error("Error: particle dt file %s is corrupt.", timestep_filename );
 				} else {
 					dt_endian = 1;
