b504dc3b4389417a07987b58c0291c8c44bd9dfc
Marcel Zemp <mzemp@pku.edu.cn>
2010-03-23 20:33:47 +0000

    Added additional check for the cases when zero1 & zero2 have a value of 0.1234.

--- utils/hfind/hfind.f
+++ utils/hfind/hfind.f
@@ -2005,7 +2005,9 @@ c.... read control information and check whether it has proper structure
        stop
       endif
 
-      if(zero1.eq.0.0 .and. zero2.eq.0.0 .and.DelDC.ne.0.0) then
+      if((( zero1 .eq. 0.0 ) .or. ( zero1 .eq. 0.1234 )) .and.
+     &     (( zero2 .eq. 0.0 ) .or. ( zero2 .eq. 0.1234 )) .and.
+     &     ( DelDC .ne. 0.0 )) then
          AEXPN = abox
       endif
 
