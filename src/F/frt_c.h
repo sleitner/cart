d2c1cdeaeb07158fe6206578a1fa4e67d4a84db2
Nick Gnedin <gnedin@fnal.gov>
2011-11-26 17:04:39 +0000

    Working commit

--- src/F/frt_c.h
+++ src/F/frt_c.h
@@ -23,9 +23,9 @@ void frtCall(initvar)(frt_real *var);
 void frtCall(cooloff)(frt_real *var, frt_real *rawrf, frt_real *time, frt_intg *info);
 void frtCall(initrun)();
 void frtCall(initrun2)(frt_real *Yp, frt_real *Tmin, frt_real *D2Gmin, frt_real *ClumpH2, frt_real *CohLenH2, frt_real *fGal, frt_real *fQSO, frt_intg *IPOP, frt_intg *IREC, frt_intg *IOUNIT);
-void frtCall(stepbegin)(frt_real *uDen, frt_real *uLen, frt_real *uTime, frt_real *dtStep, frt_real *aExp, frt_real *Hubble, frt_real *parAvg);
-void frtCall(stepend)(frt_real *vol, frt_real *parAvg);
-void frtCall(updatetables)(frt_real *rfAvg, frt_real *otAvg);
+void frtCall(stepbegin)(frt_real *uDen, frt_real *uLen, frt_real *uTime, frt_real *aExp, frt_real *Hubble, frt_real *daExp);
+void frtCall(stepend)(frt_real *dt, frt_real *vol, frt_real *par);
+void frtCall(updatetables)(frt_real *rfAvg);
 
 frt_real frtCall(tem)(frt_real *var);
 void frtCall(getcoolingrate)(frt_real *var, frt_real *rawrf, frt_real *rateCool, frt_real *rateHeat);
@@ -39,13 +39,14 @@ void frtCall(unpackradiationfields)(frt_intg *n, frt_real *data);
 
 #ifdef RT_TRANSFER
 void frtCall(initruntransfer)(frt_intg *nfreq, frt_intg *ncomp);
-void frtCall(stepbegintransfer)(frt_real *abcAvg);
 void frtCall(stependtransfer)(frt_real *abcAvg);
 
+frt_real frtCall(transferglobalac)(frt_intg *idx, frt_real *abc);
+
 #ifdef RT_ABSORPTION_CALLBACK_FULL
-void frtCall(transfercomputecellabs)(frt_intg *L, frt_real *denB, frt_real *denHI, frt_real *denHeI, frt_real *denHeII, frt_real *denMH, frt_real *dx, frt_real *abc, frt_real *var);
+void frtCall(transfercomputecellabs)(frt_intg *idx, frt_real *denB, frt_real *denHI, frt_real *denHeI, frt_real *denHeII, frt_real *denMH, frt_real *dx, frt_real *abc, frt_real *var);
 #else
-void frtCall(transfercomputecellabs)(frt_intg *L, frt_real *denB, frt_real *denHI, frt_real *denHeI, frt_real *denHeII, frt_real *denMH, frt_real *dx, frt_real *abc, frt_real *Zsol);
+void frtCall(transfercomputecellabs)(frt_intg *idx, frt_real *denB, frt_real *denHI, frt_real *denHeI, frt_real *denHeII, frt_real *denMH, frt_real *dx, frt_real *abc, frt_real *Zsol);
 #endif
 #endif
 
