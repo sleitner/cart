2d18c9e0a3bef0ac8c8c3a85b9049fc276ca857e
Nick Gnedin <gnedin@fnal.gov>
2012-03-01 02:03:08 +0000

    just a backup

---
+++ src/drivers/compute_engine.h
@@ -0,0 +1,12 @@
+#ifndef __COMPUTE_ENGINE_H__
+#define __COMPUTE_ENGINE_H__
+
+#ifndef CONFIGURED
+#error "Missing config.h include."
+#endif
+
+
+void run(int restart, const char *restart_label);
+
+
+#endif  /* __COMPUTE_ENGINE_H__ */
