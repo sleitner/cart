95c14149bcbdca4c53a44b79cdd8192890b6992d
Sam Leitner <sam.leitner@gmail.com>
2013-03-25 17:03:39 -0400

    moved models back into sf_ for hart/hart-old

--- src/sf/sf_formstar.hart.c
+++ src/sf/sf_formstar.hart.c
@@ -12,28 +12,33 @@
 #include "tree.h"
 #include "units.h"
 
-#include "models/form_star.hart.h"
-
 extern double sf_sampling_timescale; /* just check sf_timescale is >2x longer */
-extern double sf_timescale;                 /* in yrs; used to be called tau_SF, did not exist in HART */
-extern double sf_min_stellar_particle_mass;   /* in Msun; used to be called dm_star_min */
-extern double mstar_min;
-extern double dt_SF;
+
+double sf_timescale = 3.0e7;                 /* in yrs; used to be called tau_SF, did not exist in HART */
+double sf_min_stellar_particle_mass = 0.0;   /* in Msun; used to be called dm_star_min */
+double mstar_min;
+double dt_SF;
 void star_form_config_init()
 {
-    formstar_hart_config_init();
+    control_parameter_add4(control_parameter_double,&sf_min_stellar_particle_mass,"sf:min-stellar-particle-mass","sf_min_stellar_particle_mass","sf_min_stellar_particle_mass","dm_star_min","minimum mass for a newly created stellar particle, in solar masses. This value should be small enough to avoid artifically boosting the SFR in the low density gas.");
+
+    control_parameter_add3(control_parameter_time,&sf_timescale,"sf:timescale","sf_timescale","tau_SF","the timescale for star formation. Star formation in a given cell is assumed to continue with the constant rate for that period of time.");
 }
 
 
 void star_form_config_verify()
 {
-    formstar_hart_config_verify();
+    VERIFY(sf:timescale, sf_timescale >= 2*sf_sampling_timescale );
+
+    VERIFY(sf:min-stellar-particle-mass, !(sf_min_stellar_particle_mass < 0.0) );
 }
 
 
 void star_form_setup(int level)
 {
-    formstar_hart_setup(level);
+    mstar_min = sf_min_stellar_particle_mass * constants->Msun / units->mass; 
+    
+    dt_SF = sf_timescale * constants->yr / units->time;
 }
 
 void star_form_particles(int level, int icell, double dtl, double dt_eff, float sfr){
