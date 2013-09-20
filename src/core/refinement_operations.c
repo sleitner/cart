ae0bfe930d5bdb163edf27d00fb1c06bed82f3aa
Douglas Rudd <drudd@uchicago.edu>
2013-09-20 12:55:04 -0500

    formatting changes

--- src/core/refinement_operations.c
+++ src/core/refinement_operations.c
@@ -44,7 +44,7 @@ double cell_internal_energy( int icell ) {
 #endif /* HYDRO */
 
 int split ( int cell ) {
-        int i, j, ind;
+	int i, j, ind;
 	int neighbor, cell_number;
 	int result;
 	int child_cell;
@@ -54,19 +54,19 @@ int split ( int cell ) {
 	double moment;	
 #ifdef HYDRO
 	double weights[num_hydro_vars-nDim];
-        double weightextP;
+	double weightextP;
 #endif /* HYDRO */
 
 	cart_assert( cell >= 0 && cell < num_cells );
 
 #ifdef HYDRO
-        weightextP = 0;
+	weightextP = 0;
 	for ( i = 0; i < num_hydro_vars-nDim; i++ ) {
 		weights[i] = 0.0;
 	}
 #endif /* HYDRO */
 
-        /* check +/- refinement criterion */
+	/* check +/- refinement criterion */
 	if ( cell_level(cell) > min_level ) {
 		cell_number = cell_child_number(cell);
 		for ( i = 0; i < nDim; i++ ) {
@@ -105,7 +105,7 @@ int split ( int cell ) {
 			/* interpolate density,  pressure and internal energy */
 			cell_gas_density(child_cell) = cell_interpolate_with_neighbors( cell, HVAR_GAS_DENSITY, neighbors );
 			weights[1] += cell_gas_density(child_cell);
-	
+
 			cell_gas_pressure(child_cell) = cell_interpolate_with_neighbors( cell, HVAR_PRESSURE, neighbors );
 			weights[2] += cell_gas_pressure(child_cell);
 
@@ -118,8 +118,8 @@ int split ( int cell ) {
 			/* interpolate potential and add to kinetic to get total energy */
 
 			/*
-			  ASK DOUG WHY WE DO IT THAT WAY
-			*/
+			   ASK DOUG WHY WE DO IT THAT WAY
+			   */
 			cell_gas_energy(child_cell) = cell_gas_kinetic_energy(child_cell) + cell_gas_internal_energy(child_cell);
 			//				cell_interpolate_function_with_neighbors( cell, cell_internal_energy, neighbors );
 			weights[4] += cell_gas_energy(child_cell);
@@ -128,17 +128,17 @@ int split ( int cell ) {
 			cell_electron_internal_energy(child_cell) = cell_interpolate_with_neighbors( cell, HVAR_ELECTRON_INTERNAL_ENERGY, neighbors );
 			weights[5] += cell_electron_internal_energy(child_cell);
 #endif /* ELECTRON_ION_NONEQUILIBRIUM */
-                        
+
 			for ( j = 0; j < num_extra_energy_variables; j++ ) {
-                                cell_extra_energy_variables(child_cell,j) = 
-                                    cell_interpolate_with_neighbors( cell, HVAR_EXTRA_ENERGY_VARIABLES+j, neighbors );
-                                weights[j+5+num_electronion_noneq_vars] +=  cell_extra_energy_variables(child_cell,j);
+				cell_extra_energy_variables(child_cell,j) = 
+					cell_interpolate_with_neighbors( cell, HVAR_EXTRA_ENERGY_VARIABLES+j, neighbors );
+				weights[j+5+num_electronion_noneq_vars] +=  cell_extra_energy_variables(child_cell,j);
 			}
 
 #ifdef EXTRA_PRESSURE_SOURCE
 			cell_extra_pressure_source(child_cell) = cell_interpolate_with_neighbors( 
-                            cell, VAR_EXTRA_PRESSURE_SOURCE, neighbors );
-                        weightextP += cell_extra_pressure_source(child_cell);
+					cell, VAR_EXTRA_PRESSURE_SOURCE, neighbors );
+			weightextP += cell_extra_pressure_source(child_cell);
 #endif
 
 			for ( j = 0; j < num_chem_species; j++ ) {
@@ -181,13 +181,13 @@ int split ( int cell ) {
 #ifdef ELECTRON_ION_NONEQUILIBRIUM
 		weights[5] = (weights[5] == 0.0) ? 0.0: (double)num_children * (double)cell_electron_internal_energy(cell) / weights[5];
 #endif /* ELECTRON_ION_NONEQUILIBRIUM */
-                for ( j = 0; j < num_extra_energy_variables; j++ ) {
-                    ind = j+5+num_electronion_noneq_vars;
-                    weights[ind] = (weights[ind] == 0.0) ? 0.0: 
-                        (double)num_children *
-                        (double)cell_extra_energy_variables(cell,j) / 
-                        weights[ind];
-                }
+		for ( j = 0; j < num_extra_energy_variables; j++ ) {
+			ind = j+5+num_electronion_noneq_vars;
+			weights[ind] = (weights[ind] == 0.0) ? 0.0: 
+				(double)num_children *
+				(double)cell_extra_energy_variables(cell,j) / 
+				weights[ind];
+		}
 #ifdef EXTRA_PRESSURE_SOURCE
 		weightextP = (weightextP == 0.0) ? 0.0: (double)num_children * (double)cell_extra_pressure_source(cell) / weightextP;
 #endif 
@@ -195,20 +195,20 @@ int split ( int cell ) {
 		for ( j = 0; j < num_chem_species; j++ ) {
 			weights[num_hydro_vars-num_chem_species-nDim+j] = 
 				(weights[num_hydro_vars-num_chem_species-nDim+j] == 0.0) ? 0.0: 
-					(double)num_children * (double)cell_advected_variable(cell,j) / 
-					weights[num_hydro_vars-num_chem_species-nDim+j];
+				(double)num_children * (double)cell_advected_variable(cell,j) / 
+				weights[num_hydro_vars-num_chem_species-nDim+j];
 		}
-	
+
 		/* enforce conservation laws in children */
 		mass = 0.0;
 		for ( i = 0; i < num_children; i++ ) {
 			child_cell = cell_child(cell, i);
 			cart_assert( child_cell >= 0 && child_cell < num_cells );
-		
+
 			for ( j = 0; j < nDim; j++ ) {
 				cell_momentum(child_cell,j) *= weights[0];
 			}
-			
+
 			cell_gas_density(child_cell) *= weights[1];
 			cell_gas_pressure(child_cell) *= weights[2];
 			cell_gas_internal_energy(child_cell) *= weights[3];
@@ -222,7 +222,7 @@ int split ( int cell ) {
 				cell_extra_energy_variables(child_cell,j) *= weights[j+5+num_electronion_noneq_vars];
 			}
 #ifdef EXTRA_PRESSURE_SOURCE
-                        cell_extra_pressure_source(child_cell) *= weightextP;
+			cell_extra_pressure_source(child_cell) *= weightextP;
 #endif
 
 			for ( j = 0; j < num_chem_species; j++ ) {
@@ -234,12 +234,12 @@ int split ( int cell ) {
 
 		if ( mass>0.0 && fabs( mass - cell_gas_density(cell)*cell_volume[cell_level(cell)] )/mass > 1e-6 ) {
 			cart_error("Error in mass conservation in split_cell: %e %e\n", mass,
-				cell_gas_density(cell)*cell_volume[cell_level(cell)] );
+					cell_gas_density(cell)*cell_volume[cell_level(cell)] );
 		}
 
 		/* ensure new values correspond to variable limits */
 		hydro_magic_one_cell(cell);
-	
+
 #endif /* HYDRO */
 
 	} else {
